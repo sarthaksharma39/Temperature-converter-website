8e588b8540b30d3fad8abe89de23bd2f1fbf4f4d
Sarthak sharma <107210105+sarthaksharma39@users.noreply.github.com>
2023-06-17 00:15:55 +0530

    Update object.js
    Created function for temperature conversion

--- object.js
+++ object.js
@@ -1,4 +1,26 @@
-var x= new file();
-var y= new file();
-var time= x+y;
-console.log(time);
+//celsius to fahrenheit
+function CtoF() {
+    let x = document.getElementById("getdata").value;
+    let y = x * (9 / 5) + 32;
+    document.getElementById("demo").innerHTML = y;
+};
+// fahrenheit to celsius
+function FtoC() {
+    let y = 122;
+    let x = (y-32)*5/9;
+    document.getElementById("demo").innerHTML = x;
+};
+
+
+//celsius to Kelvin
+function CtoK() {
+    let x = 50;
+    let y = x + 273.15;
+    document.getElementById("demo").innerHTML = y;
+};
+//Kelvin to celsius
+function KtoC() {
+    let y = 323.15;
+    let x = y - 273.15;
+    document.getElementById("demo").innerHTML = x;
+};
